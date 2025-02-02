19749c8a0d8a9d653e57a5d045c27c4e367f88f0
marsdong3 <marsdong3@gmail.com>
2025-02-02 10:04:20 +0900

    .

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # static
+
+
+https://github.com/marsdong3/static
+
+
+https://marsdong3.github.io/static/
--- todo.js
+++ todo.js
@@ -20,6 +20,50 @@ const appendTodos = (text) => {
     paintTodos();
 }
 
+const deleteTodo = (todoId) => {
+    const newTodos = getAllTodos().filter(todo => todo.id !== todoId );
+    setTodos(newTodos);
+    paintTodos()
+}
+
+const completeTodo = (todoId) => {
+    const newTodos = getAllTodos().map(todo => todo.id === todoId ? {...todo,  isCompleted: !todo.isCompleted} : todo )
+    setTodos(newTodos);
+    paintTodos();
+}
+
+const updateTodo = (text, todoId) => {
+    const currentTodos = getAllTodos();
+    const newTodos = currentTodos.map(todo => todo.id === todoId ? ({...todo, content: text}) : todo);
+    setTodos(newTodos);
+    paintTodos();
+}
+
+const onDbclickTodo = (e, todoId) => {
+    const todoElem = e.target;
+    const inputText = e.target.innerText;
+    const todoItemElem = todoElem.parentNode;
+    const inputElem = document.createElement('input');
+    inputElem.value = inputText;
+    inputElem.classList.add('edit-input');
+    inputElem.addEventListener('keypress', (e)=>{
+        if(e.key === 'Enter') {
+            updateTodo(e.target.value, todoId);
+            document.body.removeEventListener('click', onClickBody );
+        }
+    })
+
+    const onClickBody = (e) => {
+        if(e.target !== inputElem)  {
+            todoItemElem.removeChild(inputElem);
+            document.body.removeEventListener('click', onClickBody );
+        }
+    }
+
+    document.body.addEventListener('click', onClickBody)
+    todoItemElem.appendChild(inputElem);
+}
+
 const paintTodos = () => {
     todoListElem.innerHTML = ''; //todoListElem 요소 안의 HTML 초기화
 	const allTodos = getAllTodos() // todos 배열 가져오기
@@ -28,20 +72,23 @@ const paintTodos = () => {
         const todoItemElem = document.createElement('li');
         todoItemElem.classList.add('todo-item');
 
-        // todoItemElem.setAttribute('data-id', todo.id );
+        todoItemElem.setAttribute('data-id', todo.id );
 
         const checkboxElem = document.createElement('div');
         checkboxElem.classList.add('checkbox');
+        checkboxElem.addEventListener('click', () => completeTodo(todo.id))
 
         const todoElem = document.createElement('div');
         todoElem.classList.add('todo');
+        todoElem.addEventListener('dblclick', (event) => onDbclickTodo(event, todo.id))
         todoElem.innerText = todo.content;
 
         const delBtnElem = document.createElement('button');
         delBtnElem.classList.add('delBtn');
+        delBtnElem.addEventListener('click', () =>  deleteTodo(todo.id))
         delBtnElem.innerHTML = 'X';
 
-        if(todo.isChecked) {
+        if(todo.isCompleted) {
             todoItemElem.classList.add('checked');
             checkboxElem.innerText = '✔';
         }
